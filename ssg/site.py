9163442ae98787830ed4c9bdadeb4050c55a543a
Rhobar9 <olo.k97@wp.pl>
2022-07-17 16:38:35 +0200

    Module1_after_repairs

--- ssg/site.py
+++ ssg/site.py
@@ -7,7 +7,7 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest / path.relative_to(self.source)
+        directory = self.dest/path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
@@ -15,7 +15,3 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
-
-
-
